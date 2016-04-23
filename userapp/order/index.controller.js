f2da652a3ef728be6d5ce5bdc65d73bdc942d5c4
Ju, Xing-Xing <StarRUC@StarRUCs-MacBook-Pro.local>
2016-04-22 23:53:32 -0700

    Xingxing Ju - Fix the bug for Order History Data Access

--- userapp/order/index.controller.js
+++ userapp/order/index.controller.js
@@ -5,13 +5,14 @@
         .module('app')
         .controller('Order.IndexController', Controller);
 
-    function Controller($window, UserService, OrderService, FlashService) {
+    function Controller($window, $scope, UserService, OrderService, FlashService) {
         var vm = this;
 
         vm.user = null;
         vm.orders = null;
-        //vm.saveUser = saveUser;
-        //vm.deleteUser = deleteUser;
+        //vm.showComment = showComment;
+        //vm.showModal = false;
+        //vm.toggleModal = toggleModal;
 
         initController();
 
@@ -24,28 +25,80 @@
                 });
             });
 
+            // Modal
+            //$scope.showModal = false;
+            //$scope.toggleModal = function(){
+            //    $scope.showModal = !$scope.showModal;
+            //};
         }
 
-        //function saveUser() {
-        //    UserService.Update(vm.user)
-        //        .then(function () {
-        //            FlashService.Success('User updated');
-        //        })
-        //        .catch(function (error) {
-        //            FlashService.Error(error);
-        //        });
+        //function toggleModal() {
+        //    vm.showModal = !vm.showModal;
         //}
         //
-        //function deleteUser() {
-        //    UserService.Delete(vm.user._id)
-        //        .then(function () {
-        //            // log user out
-        //            $window.location = '/login';
-        //        })
-        //        .catch(function (error) {
-        //            FlashService.Error(error);
-        //        });
+        //function showComment() {
+        //    //UserService.Update(vm.user)
+        //    //    .then(function () {
+        //    //        FlashService.Success('User updated');
+        //    //    })
+        //    //    .catch(function (error) {
+        //    //        FlashService.Error(error);
+        //    //    });
+        //    $('#orderModel').modal('show');
+        //
         //}
+
     }
+
+    //var mymodal = angular.module('mymodal', []);
+    //
+    //mymodal.controller('MainCtrl', function ($scope) {
+    //    $scope.showModal = false;
+    //    $scope.toggleModal = function(){
+    //        $scope.showModal = !$scope.showModal;
+    //    };
+    //});
+    //
+    //mymodal.directive('modal', function () {
+    //    return {
+    //        template: '<div class="modal fade">' +
+    //        '<div class="modal-dialog">' +
+    //        '<div class="modal-content">' +
+    //        '<div class="modal-header">' +
+    //        '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+    //        '<h4 class="modal-title">{{ title }}</h4>' +
+    //        '</div>' +
+    //        '<div class="modal-body" ng-transclude></div>' +
+    //        '</div>' +
+    //        '</div>' +
+    //        '</div>',
+    //        restrict: 'E',
+    //        transclude: true,
+    //        replace:true,
+    //        scope:true,
+    //        link: function postLink(scope, element, attrs) {
+    //            scope.title = attrs.title;
+    //
+    //            scope.$watch(attrs.visible, function(value){
+    //                if(value == true)
+    //                    $(element).modal('show');
+    //                else
+    //                    $(element).modal('hide');
+    //            });
+    //
+    //            $(element).on('shown.bs.modal', function(){
+    //                scope.$apply(function(){
+    //                    scope.$parent[attrs.visible] = true;
+    //                });
+    //            });
+    //
+    //            $(element).on('hidden.bs.modal', function(){
+    //                scope.$apply(function(){
+    //                    scope.$parent[attrs.visible] = false;
+    //                });
+    //            });
+    //        }
+    //    };
+    //});
 
 })();
