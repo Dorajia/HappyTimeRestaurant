446e06d69adb010f07f1b320b11bd55946efc1b9
StarRUC <juxingxing@gmail.com>
2016-05-03 10:56:33 -0700

    Fix issue of comment more dishes than first one

--- app/order/index.controller.js
+++ app/order/index.controller.js
@@ -5,7 +5,7 @@
         .module('app')
         .controller('Order.IndexController', Controller);
 
-    function Controller(OrderService, FlashService) {
+    function Controller($scope, OrderService, FlashService) {
         var vm = this;
 
         vm.user = null;
@@ -14,6 +14,7 @@
         vm.currentDate = null;
         vm.confirmOrderData = null;
         vm.commentOrderData = null;
+        vm.comments = [];
         vm.showModal = showModal;
         vm.hideModal = hideModal;
         vm.confirmOrder = confirmOrder;
@@ -41,15 +42,20 @@
 
         function confirmOrder(order_id) {
             OrderService.ConfirmOrder(order_id).then(function (orderResult) {
-                vm.orders = orderResult.data;
+                //vm.orders = orderResult.data;
+                return FlashService.Success("Confirm Delivery successfully!");
             });
         }
 
-        function submitComment(order_id, dish_id, comment) {
-            OrderService.SubmitComment(order_id, dish_id, comment).then(function (result) {
+        //function submitComment(order_id, dish_id, comment) {
+        //    OrderService.SubmitComment(order_id, dish_id, comment).then(function (result) {
+        function submitComment(dish, index) {
+            OrderService.SubmitComment(vm.commentOrderData._id, dish.name, vm.comments[index]).then(function (result) {
+            //OrderService.SubmitComment(vm.commentOrderData._id, dish.name, $('input[name=("comment"+index)]').val()).then(function (result) {
                 if (result.sucess == true) {
-                    //return FlashService.Success("Comment post successfully!");
-                    $dialogs.notify('Something Happened!','Comment post successfully!');
+                    dish.comment = null;
+                    return FlashService.Success("Comment post successfully!");
+                    //$dialogs.notify('Something Happened!','Comment post successfully!');
                 }
                 else {
                     return FlashService.Error("Comment post failed!");
