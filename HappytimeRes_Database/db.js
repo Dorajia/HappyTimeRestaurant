f3f0b010bfb5533fea4f6ab158a2b86141c258dc
Dorajia <yuanyuan.jia@sjsu.edu>
2016-05-03 23:00:45 +0000

    change cart

--- HappytimeRes_Database/db.js
+++ HappytimeRes_Database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	'url' : 'mongodb://ec2-52-34-52-175.us-west-2.compute.amazonaws.com:27018,ec2-52-35-71-66.us-west-2.compute.amazonaws.com:27018,ec2-52-39-40-252.us-west-2.compute.amazonaws.com:27018/HappyTimeRestuarant?replicaSet=test&readPreference=secondaryPreferred'
+//	'url' : 'mongodb://ec2-52-34-52-175.us-west-2.compute.amazonaws.com:27018,ec2-52-35-71-66.us-west-2.compute.amazonaws.com:27018,ec2-52-39-40-252.us-west-2.compute.amazonaws.com:27018/HappyTimeRestuarant?replicaSet=test&readPreference=secondaryPreferred'
+    'url' : 'mongodb://ec2-52-11-87-42.us-west-2.compute.amazonaws.com:27017/test'
 };
