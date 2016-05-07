19a675bedefa01d128cd106a35b788e028252047
Dorajia <yuanyuan.jia@sjsu.edu>
2016-05-07 01:37:56 +0000

    update time format

--- HappytimeRes_Database/models/comment.js
+++ HappytimeRes_Database/models/comment.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var timestamps = require('mongoose-createdat-updatedat');
+//var timestamps = require('mongoose-createdat-updatedat');
+var moment = require('moment-timezone');
+
 
 var CommentSchema = new Schema(
 {
@@ -9,10 +11,19 @@ var CommentSchema = new Schema(
 	dish_name:{type:String, ref:'dish'},
 	score:Number,
 	comment_description: String,
-	order_id: String
+	order_id: String,
+	createdAt:String
 });
 
 //automaticall add createdat and updateat in the schema.
-CommentSchema.plugin(timestamps);
+//CommentSchema.plugin(timestamps);
+
+CommentSchema.pre('save', function (next) {
+	 var format = 'YYYY/MM/DD HH:mm:ss ZZ';
+    this.createdAt = moment().tz("America/Los_Angeles").format(format);
+    next();
+});
+
+
 
 module.exports = mongoose.model('comment', CommentSchema,'comment');
